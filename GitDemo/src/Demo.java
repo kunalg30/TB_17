650d9b0917b6de92541dbc43ae8e38e32e0650a5
kunalg30 <115782824+kunalg30@users.noreply.github.com>
2022-10-15 13:29:41 +0530

    Update Demo.java

--- GitDemo/src/Demo.java
+++ GitDemo/src/Demo.java
@@ -1,8 +1,16 @@
 
 public class Demo {
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		System.out.println("Hello");
 		System.out.println("Hello dono rushikesh");
+	}
+	public void m1()
+	{
+	
+	System.out.println("Hello");
+		System.out.println("Hello dono rushikesh");
+	
 	}
 
 }
