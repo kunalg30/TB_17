5bd004a0523926c61fa89f96498deb44e1648acb
Rushikeshkudekar <115785641+Rushikeshkudekar@users.noreply.github.com>
2022-10-15 13:21:09 +0530

    Update Demo.java

--- GitDemo/src/Demo.java
+++ GitDemo/src/Demo.java
@@ -3,6 +3,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello dono rushikesh");
+		System.out.println("rushikudekar make some chenges"):
 	}
 
 }
